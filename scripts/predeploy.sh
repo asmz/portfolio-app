dce54ff722143802a1c230fd50d1410252488abe
asmz <land.front.high.field@gmail.com>
2025-02-11 10:34:49 +0900

    fix

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -4,7 +4,9 @@ mv dist/assets/node_modules/* dist/assets/
 case $(uname -s) in
 Darwin)
   find dist/_expo/static/js/web/ -type f -print0 | xargs -0 sed -i '' 's/assets\/node_modules/assets/g'
+  ;;
 *)
   find dist/_expo/static/js/web/ -type f -print0 | xargs -0 sed -i 's/assets\/node_modules/assets/g'
+  ;;
 esac
 
