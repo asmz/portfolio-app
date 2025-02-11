3dbc109623cf927ddef7439503c014daab754867
asmz <land.front.high.field@gmail.com>
2025-02-11 10:25:56 +0900

    fix: sedのOS依存吸収

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 expo export -p web
 mv dist/assets/node_modules/* dist/assets/
-find dist/_expo/static/js/web/ -type f -print0 | xargs -0 sed -i '' 's/assets\/node_modules/assets/g'
+find dist/_expo/static/js/web/ -type f -print0 | xargs -0 sed -i .bak 's/assets\/node_modules/assets/g'
